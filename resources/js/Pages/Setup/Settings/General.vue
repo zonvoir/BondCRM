669ff49dae833e5199407c129f273cd0f6be3339
Abhishek Sharma <abhishek@zonvoir.com>
2025-09-04 18:45:28 +0530

    fix the General Setting Design

--- resources/js/Pages/Setup/Settings/General.vue
+++ resources/js/Pages/Setup/Settings/General.vue
@@ -2,13 +2,12 @@
 import SettingsLayout from '@/Layouts/SettingsLayout.vue';
 import AppLayout from '@/Layouts/AppLayout.vue';
 import PanelLayout from '@/Layouts/PanelLayout.vue';
-import CommonFile from '@/Components/Common/CommonFile.vue';
 import { useForm } from '@inertiajs/vue3';
 import CommonInput from '@/Components/Common/CommonInput.vue';
 import CommonButton from '@/Components/Common/CommonButton.vue';
 import { ref } from 'vue';
 import { useCustomToast } from '@/Composables/useToast';
-import CommonCard from '@/Components/Common/CommonCard.vue';
+import ImageUpload from '@/Components/ImageUpload.vue';
 const { showToast } = useCustomToast();
 
 const props = defineProps({
@@ -96,139 +95,181 @@ const cronRunSubmit = () => {
         <PanelLayout>
             <SettingsLayout :menu="menuSettings">
                 <form method="post">
-                    <div class="flex flex-wrap justify-start gap-3">
+                    <div class="flex flex-col gap-3">
                         <div>
-                            <CommonFile
-                                label="Icon Logo Dark (512 x 512)"
-                                acceptedFormats="image/png"
-                                required
-                                v-model="form.iconLogoDark"
-                                :error="form.errors.iconLogoDark"
-                                :image="generalSettings?.data?.icon_logo_dark"
-                            />
-                        </div>
-                        <div>
-                            <CommonFile
-                                label="Icon Logo White (512 x 512)"
-                                acceptedFormats="image/png"
-                                required
-                                v-model="form.iconLogoWhite"
-                                :error="form.errors.iconLogoWhite"
-                                :image="generalSettings?.data?.icon_logo_white"
-                            />
-                        </div>
-                        <div>
-                            <CommonFile
-                                label="Logo Dark (300 x 55)"
-                                acceptedFormats="image/png"
-                                required
-                                v-model="form.logoDark"
-                                :error="form.errors.logoDark"
-                                :image="generalSettings?.data?.logo_dark"
-                            />
-                        </div>
-                        <div>
-                            <CommonFile
-                                label="Logo White (300 x 55)"
-                                acceptedFormats="image/png"
-                                required
-                                v-model="form.logoWhite"
-                                :error="form.errors.logoWhite"
-                                :image="generalSettings?.data?.logo_white"
-                            />
-                        </div>
-                        <div>
-                            <CommonFile
-                                label="Favicon (40 x 40)"
-                                acceptedFormats="image/png"
-                                required
-                                v-model="form.favicon"
-                                :error="form.errors.favicon"
-                                :image="generalSettings?.data?.favicon"
-                            />
-                        </div>
-                    </div>
-                    <div class="col-span-12 my-3">
-                        <CommonInput
-                            label="Company Name"
-                            required
-                            v-model="form.companyName"
-                            :error="form.errors.companyName"
-                        />
-                    </div>
+                            <div class="flex items-center justify-between">
+                                <div>
+                                    <h1
+                                        class="pt-2 text-base font-semibold text-gray-600 dark:text-gray-100"
+                                    >
+                                        Company Info
+                                    </h1>
+                                    <h6 class="pb-3 text-sm text-gray-500">
+                                        Provide the company information below
+                                    </h6>
+                                </div>
+                                <div>
+                                    <div class="flex justify-start py-5">
+                                        <CommonButton
+                                            :processing="form.processing"
+                                            variant="primary"
+                                            @click="submit"
+                                        >
+                                            Save Changes
+                                        </CommonButton>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr />
+                            <div
+                                class="flex items-center justify-between gap-3 py-4"
+                            >
+                                <div class="w-full">
+                                    <CommonInput
+                                        label="Company Name"
+                                        required
+                                        v-model="form.companyName"
+                                        :error="form.errors.companyName"
+                                    />
+                                </div>
 
-                    <div class="col-span-12 my-3">
-                        <CommonInput
-                            label="Allowed file types"
-                            required
-                            v-model="form.allowFileTypes"
-                            :error="form.errors.allowFileTypes"
-                        />
-                    </div>
+                                <div class="w-full">
+                                    <CommonInput
+                                        label="Allowed file types"
+                                        required
+                                        v-model="form.allowFileTypes"
+                                        :error="form.errors.allowFileTypes"
+                                    />
+                                </div>
+                            </div>
+                        </div>
 
-                    <div class="flex justify-start py-5">
-                        <CommonButton
-                            :processing="form.processing"
-                            variant="primary"
-                            @click="submit"
+                        <div class="pt-4">
+                            <div class="pb-4">
+                                <h1
+                                    class="text-base font-semibold text-gray-600 dark:text-gray-100"
+                                >
+                                    Company Images
+                                </h1>
+                                <h6 class="pb-3 text-sm text-gray-500">
+                                    Provide the Company Images
+                                </h6>
+                                <hr />
+                            </div>
+                            <div class="grid grid-cols-1 gap-3 sm:grid-cols-2">
+                                <ImageUpload
+                                    label="Icon Logo Dark (512 x 512)"
+                                    acceptedFormats="image/png"
+                                    required
+                                    v-model="form.iconLogoDark"
+                                    :error="form.errors.iconLogoDark"
+                                    :image="
+                                        generalSettings?.data?.icon_logo_dark
+                                    "
+                                />
+                                <ImageUpload
+                                    label="Icon Logo White (512 x 512)"
+                                    acceptedFormats="image/png"
+                                    required
+                                    v-model="form.iconLogoWhite"
+                                    :error="form.errors.iconLogoWhite"
+                                    :image="
+                                        generalSettings?.data?.icon_logo_white
+                                    "
+                                />
+                                <ImageUpload
+                                    label="Logo Dark (300 x 55)"
+                                    acceptedFormats="image/png"
+                                    required
+                                    v-model="form.logoDark"
+                                    :error="form.errors.logoDark"
+                                    :image="generalSettings?.data?.logo_dark"
+                                />
+                                <ImageUpload
+                                    label="Logo White (300 x 55)"
+                                    acceptedFormats="image/png"
+                                    required
+                                    v-model="form.logoWhite"
+                                    :error="form.errors.logoWhite"
+                                    :image="generalSettings?.data?.logo_white"
+                                />
+                                <ImageUpload
+                                    label="Favicon (40 x 40)"
+                                    acceptedFormats="image/png"
+                                    required
+                                    v-model="form.favicon"
+                                    :error="form.errors.favicon"
+                                    :image="generalSettings?.data?.favicon"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <div class="pt-6">
+                    <div>
+                        <h1
+                            class="text-base font-semibold text-gray-600 dark:text-gray-100"
                         >
-                            Save Changes
-                        </CommonButton>
+                            Application Utilities
+                        </h1>
+                        <h6 class="pb-3 text-sm text-gray-500">
+                            Quick actions for cache, storage & cron
+                        </h6>
+                        <hr />
                     </div>
-                </form>
-
-                <form method="post">
-                    <div class="grid grid-cols-12 gap-3">
-                        <div class="col-span-12 sm:col-span-4">
-                            <CommonButton
-                                v-tooltip.top="
-                                    'Clear the application cache and optimize the system.'
-                                "
-                                class="w-full"
-                                variant="primary"
-                                icon="heroicons-outline:arrow-path"
-                                @click="clearCacheSubmit"
-                                :processing="clearCache"
-                            >
-                                Clear Cache
-                            </CommonButton>
+                    <form method="post">
+                        <div class="grid grid-cols-12 gap-3">
+                            <div class="col-span-12 sm:col-span-4">
+                                <CommonButton
+                                    v-tooltip.top="
+                                        'Clear the application cache and optimize the system.'
+                                    "
+                                    class="w-full"
+                                    variant="secondary"
+                                    icon="heroicons-outline:arrow-path"
+                                    @click="clearCacheSubmit"
+                                    :processing="clearCache"
+                                >
+                                    Clear Cache
+                                </CommonButton>
+                            </div>
+                            <div class="col-span-12 sm:col-span-4">
+                                <CommonButton
+                                    v-tooltip.top="
+                                        isLink
+                                            ? 'Storage link created.'
+                                            : 'Create a symbolic link to the storage folder. This is required for file uploads to be publicly accessible.'
+                                    "
+                                    class="w-full"
+                                    variant="secondary"
+                                    :icon="
+                                        isLink
+                                            ? 'heroicons-outline:circle-stack'
+                                            : 'heroicons-outline:exclamation-circle'
+                                    "
+                                    :processing="storageLink"
+                                    @click="storageLinkSubmit"
+                                >
+                                    Storage Link
+                                </CommonButton>
+                            </div>
+                            <div class="col-span-12 sm:col-span-4">
+                                <CommonButton
+                                    v-tooltip.top="
+                                        'Run all scheduled tasks immediately. This will execute any tasks that are scheduled to run in your application.'
+                                    "
+                                    class="w-full"
+                                    variant="secondary"
+                                    icon="heroicons-outline:cpu-chip"
+                                    :processing="manuallyCronRun"
+                                    @click="cronRunSubmit"
+                                >
+                                    Manually Cron Run
+                                </CommonButton>
+                            </div>
                         </div>
-                        <div class="col-span-12 sm:col-span-4">
-                            <CommonButton
-                                v-tooltip.top="
-                                    isLink
-                                        ? 'Storage link created.'
-                                        : 'Create a symbolic link to the storage folder. This is required for file uploads to be publicly accessible.'
-                                "
-                                class="w-full"
-                                variant="primary"
-                                :icon="
-                                    isLink
-                                        ? 'heroicons-outline:circle-stack'
-                                        : 'heroicons-outline:exclamation-circle'
-                                "
-                                :processing="storageLink"
-                                @click="storageLinkSubmit"
-                            >
-                                Storage Link
-                            </CommonButton>
-                        </div>
-                        <div class="col-span-12 sm:col-span-4">
-                            <CommonButton
-                                v-tooltip.top="
-                                    'Run all scheduled tasks immediately. This will execute any tasks that are scheduled to run in your application.'
-                                "
-                                class="w-full"
-                                variant="primary"
-                                icon="heroicons-outline:cpu-chip"
-                                :processing="manuallyCronRun"
-                                @click="cronRunSubmit"
-                            >
-                                Manually Cron Run
-                            </CommonButton>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </SettingsLayout>
         </PanelLayout>
     </AppLayout>
