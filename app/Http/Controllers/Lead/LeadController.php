378ad4d148035ef5104e6187287b9f6226668913
Abhishek Sharma <abhishek@zonvoir.com>
2025-09-15 16:01:14 +0530

    Composer Lint run

--- app/Http/Controllers/Lead/LeadController.php
+++ app/Http/Controllers/Lead/LeadController.php
@@ -48,22 +48,22 @@ class LeadController extends Controller
 
     public function export(Request $request, $type)
     {
-        $filename = 'leads_' . now()->format('Y_m_d_H_i_s');
+        $filename = 'leads_'.now()->format('Y_m_d_H_i_s');
 
         return match ($type) {
             'csv' => Excel::download(
                 new LeadsExport($request),
-                $filename . '.csv',
+                $filename.'.csv',
                 ExcelFormat::CSV
             ),
             'pdf' => Excel::download(
                 new LeadsExport($request),
-                $filename . '.pdf',
+                $filename.'.pdf',
                 ExcelFormat::MPDF
             ),
             default => Excel::download(
                 new LeadsExport($request),
-                $filename . '.xlsx',
+                $filename.'.xlsx',
                 ExcelFormat::XLSX
             ),
         };
@@ -121,7 +121,7 @@ class LeadController extends Controller
     public function leadDetails(Lead $lead)
     {
         $props = [
-            
+
         ];
 
         return Inertia::render('Lead/Details', $props);
